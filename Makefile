07a9a76b839104decea5c860f00377135dfd33fb
billerz <billpatana@gmail.com>
2022-02-27 14:11:22 +0700

    add toupper

--- Makefile
+++ Makefile
@@ -15,7 +15,8 @@ SRCS = ft_isdigit.c \
 		ft_isalnum.c \
 		ft_isascii.c \
 		ft_isprint.c \
-		ft_strlen.c
+		ft_strlen.c \
+		ft_toupper.c
 
 OBJS = $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -21,5 +21,6 @@ int					ft_isalnum(int c);
 int					ft_isascii(int c);
 int					ft_isprint(int c);
 size_t				ft_strlen(const char *s);
+int					ft_toupper(int c);
 
 #endif
