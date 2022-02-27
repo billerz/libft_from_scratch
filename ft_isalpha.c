d683d9075135940c8e437b059aa1fb070b083a6e
billerz <billpatana@gmail.com>
2022-02-27 13:25:18 +0700

    add isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ppetchda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: npatthan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/19 22:00:22 by ppetchda          #+#    #+#             */
-/*   Updated: 2022/02/19 22:16:01 by ppetchda         ###   ########.fr       */
+/*   Created: 2022/02/19 22:17:29 by npatthan          #+#    #+#             */
+/*   Updated: 2022/02/19 22:21:18 by npatthan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int					ft_isdigit(int c);
-int					ft_isalpha(int c);
-
-#endif
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
